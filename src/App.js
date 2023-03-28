a0148bf67d6889560bb61119edd2f646dc8a3bf5
Julio <72631506+JulioOli@users.noreply.github.com>
2023-03-28 14:29:01 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React diferenca
         </a>
       </header>
     </div>
