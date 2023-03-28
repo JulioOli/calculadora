9986f7a0cfaf294aacd680f80948c30bec23e560
Julio <72631506+JulioOli@users.noreply.github.com>
2023-03-28 16:55:37 -0300

    ajeitando o CSS dos botões

--- src/Componentes/Calculadora.jsx
+++ src/Componentes/Calculadora.jsx
@@ -1,33 +1,39 @@
-import react from 'react'
-import './Calculadora.css'
-import Container from '@mui/material/Container';
+import react from "react";
+import "./Calculadora.css";
+import Container from "@mui/material/Container";
+import { Box } from "@mui/system";
 
-export default function Calculadora(){
-    return(
-        <Container maxWidth="xs">
-        <div className='wrapper'>
-            
-            <button>0</button>
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>AC</button>
-            <button>+/-</button>
-            <button>%</button>
-            <button>/</button>
-            <button>X</button>
-            <button>-</button>
-            <button>+</button>
-            <button>=</button>
-            <button>,</button>
+export default function Calculadora() {
+  return (
+    <div>
+      <Box className m={10} />
+      <Container maxWidth="xs">
+        <div className="wrapper">
+        <Box className m={1} />
+            <h1 className="resultado">01</h1>
+          <button className="other2">AC</button>
+          <button className="other2">+/-</button>
+          <button className="other2">%</button>
+          <button className="mainOperator">/</button>
+          <button className="other">7</button>
+          <button className="other">8</button>
+          <button className="other">9</button>
+          <button className="mainOperator">X</button>
+          <button className="other">4</button>
+          <button className="other">5</button>
+          <button className="other">6</button>
+          <button className="mainOperator">-</button>
 
+          <button className="other">1</button>
+          <button className="other">2</button>
+          <button className="other">3</button>
+
+          <button className="mainOperator">+</button>
+          <button className="other">0</button>
+          <button className="other">,</button>
+          <button className="other2">=</button>
         </div>
-        </Container>
-    )
+      </Container>
+    </div>
+  );
 }
